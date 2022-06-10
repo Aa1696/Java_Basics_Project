c857fc5669b16334201c83e67cef36abfcf4f407
Aa1696 <sonumahtoraj121@gmail.com>
2022-06-10 18:51:32 +0530

    Chabged in the main function

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,31 @@ import com.java_basics.*;
 
 public class Main {
     public static void main(String[] args) {
-        Hello_World hell= new Hello_World();
-        System.out.println(hell.hello());
+        int res=calculateHighScorePosition(1500);
+        String name="John";
+        displayHighScorePosition(name, res);
+        res=calculateHighScorePosition(900);
+        displayHighScorePosition("wick", res);
+        res=calculateHighScorePosition(400);
+        displayHighScorePosition("shyam", res);
+        res=calculateHighScorePosition(50);
+        displayHighScorePosition("rangella", res);
     }
+
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println(name + " "+"Managed to get the position" +" "+ position +" "+ "On the high score table");
+    }
+    public static int calculateHighScorePosition(int score){
+        if(score>=1000){
+            return 1;
+        } else if (score >=500 && score<1000) {
+            return 2;
+        } else if (score>=100 && score<500) {
+            return 3;
+        }
+        else{
+            return 4;
+        }
+    }
+
 }
