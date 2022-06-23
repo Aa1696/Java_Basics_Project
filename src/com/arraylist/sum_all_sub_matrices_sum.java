9188834e1bb6343c9c5f7fabca425c7c1d3422a8
sonu mahto <sonumahtoraj121@gmail.com>
2022-06-23 22:20:25 +0530

    Multihreading

--- src/com/arraylist/sum_all_sub_matrices_sum.java
+++ src/com/arraylist/sum_all_sub_matrices_sum.java
@@ -16,6 +16,5 @@ public class sum_all_sub_matrices_sum {
     public static void main(String[] args){
         int [][] A = { {1, 1},{1, 1} };
         System.out.println(sum_sub_matrice(A));
-
     }
 }
---
+++ src/com/multithreading/main_multithread.java
@@ -0,0 +1,9 @@
+package com.multithreading;
+
+public class main_multithread {
+    public static void main(String[] args){
+        multithread new_thing=new multithread();
+        new_thing.start();
+
+    }
+}
